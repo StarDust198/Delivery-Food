30360aa89dddac645f903fc99da3f4b2eca60438
StarDust85z <roadtomars2030@gmail.com>
2021-11-08 17:47:40 +0300

    cards from server

--- dist/main.js
+++ dist/main.js
@@ -1358,8 +1358,46 @@ const auth = user => {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 const partners = () => {
+  const cardsRestaurants = document.querySelector('.cards-restaurants');
+
   const renderItems = data => {
-    console.log(data);
+    data.forEach(item => {
+      const {
+        image,
+        kitchen,
+        name,
+        price,
+        products,
+        stars,
+        time_of_delivery
+      } = item,
+            a = document.createElement('a');
+      a.href = 'restaurant.html';
+      a.classList.add('card', 'card-restaurant');
+      a.dataset.products = products;
+      a.innerHTML = `
+                <img src="${image}" alt="image" class="card-image" />
+                <div class="card-text">
+                    <div class="card-heading">
+                        <h3 class="card-title">${name}</h3>
+                        <span class="card-tag tag">${time_of_delivery} мин</span>
+                    </div>
+                    <div class="card-info">
+                        <div class="rating">
+                            ${stars}
+                        </div>
+                        <div class="price">От ${price} ₽</div>
+                        <div class="category">${kitchen}</div>
+                    </div>
+                </div>
+            `;
+      a.addEventListener('click', e => {
+        e.preventDefault();
+        localStorage.setItem('restaurant', JSON.stringify(item));
+        window.location.href = '/restaurant.html';
+      });
+      cardsRestaurants.append(a);
+    });
   };
 
   fetch(`https://test-5e265-default-rtdb.firebaseio.com/db/partners.json`).then(res => res.json()).then(data => {
--- dist/menu.js
+++ dist/menu.js
@@ -1358,17 +1358,65 @@ const auth = user => {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 const menu = () => {
-  const restaurant = 'food-band';
+  const cardsMenu = document.querySelector('.cards-menu');
 
-  const renderItems = data => {
-    console.log(data);
+  const changeTitle = restaurant => {
+    const heading = document.querySelector('.section-heading'),
+          title = heading.querySelector('.restaurant-title'),
+          rating = heading.querySelector('.rating'),
+          price = heading.querySelector('.price'),
+          category = heading.querySelector('.category');
+    title.textContent = restaurant.name;
+    rating.textContent = restaurant.stars;
+    price.textContent = `От ${restaurant.price} ₽`;
+    category.textContent = restaurant.kitchen;
   };
 
-  fetch(`https://test-5e265-default-rtdb.firebaseio.com/db/${restaurant}.json`).then(res => res.json()).then(data => {
-    renderItems(data);
-  }).catch(error => {
-    console.log(error);
-  });
+  const renderItems = data => {
+    data.forEach(({
+      description,
+      id,
+      image,
+      name,
+      price
+    }) => {
+      const div = document.createElement('div');
+      div.classList.add('card');
+      div.dataset.id = id;
+      div.innerHTML = `
+                <img src="${image}" alt="image" class="card-image" />
+                <div class="card-text">
+                    <div class="card-heading">
+                        <h3 class="card-title card-title-reg">${name}</h3>
+                    </div>
+                    <div class="card-info">
+                        <div class="ingredients">${description}
+                        </div>
+                    </div>
+                    <div class="card-buttons">
+                        <button class="button button-primary button-add-cart">
+                            <span class="button-card-text">В корзину</span>
+                            <span class="button-cart-svg"></span>
+                        </button>
+                        <strong class="card-price-bold">${price} ₽</strong>
+                    </div>
+                </div>
+            `;
+      cardsMenu.append(div);
+    });
+  };
+
+  if (localStorage.getItem('restaurant')) {
+    const restaurant = JSON.parse(localStorage.getItem('restaurant'));
+    changeTitle(restaurant);
+    fetch(`https://test-5e265-default-rtdb.firebaseio.com/db/${restaurant.products}`).then(res => res.json()).then(data => {
+      renderItems(data);
+    }).catch(error => {
+      console.log(error);
+    });
+  } else {
+    window.location.href = '/';
+  }
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (menu);
